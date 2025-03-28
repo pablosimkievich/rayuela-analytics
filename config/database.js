99576c5d1c100f9c1c06d03fab4e0e82b2303105
polness <pablosimkievich@gmail.com>
2025-03-28 13:56:57 -0300

    dotenv

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('sql5754365', 'sql5754365', 'LFALEV3tDB', {
-  host: 'sql5.freesqldatabase.com',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
